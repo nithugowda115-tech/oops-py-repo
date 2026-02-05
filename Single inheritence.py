de21af870ea65e654652ee22125e0132c9690412
nithugowda115-tech <nithugowda115@gmail.com>
2026-02-05 20:43:14 +0530

    Add files via upload

---
+++ Single inheritence.py
@@ -0,0 +1,31 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display_book_details(self):
+        print("Title:", self.title)
+        print("Author:", self.author)
+
+class IssuedBook(Book):
+    def __init__(self, title, author, issued_to, issued_date):
+        super().__init__(title, author)
+        self.issued_to = issued_to
+        self.issued_date = issued_date
+
+    def display_book_details(self):
+        super().display_book_details()
+
+    def display_issued_book_details(self):
+        self.display_book_details()
+        print("Issued To:", self.issued_to)
+        print("Issued Date:", self.issued_date)
+
+book1 = IssuedBook(
+    "Clean Code",
+    "Robert C. Martin",
+    "Nithya",
+    "05-02-2026"
+)
+
+book1.display_issued_book_details()
